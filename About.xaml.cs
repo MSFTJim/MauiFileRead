8ad251f94a1d0608f6c20d1aa815aa108a398e55
MSFTJim <jamesvac@live.com>
2024-12-17 10:49:54 -0500

    added code to show read error and read properly via button

--- About.xaml.cs
+++ About.xaml.cs
@@ -5,5 +5,28 @@ public partial class About : ContentPage
 	public About()
 	{
 		InitializeComponent();
-	}
+
+        string filePath = "AboutAssets.txt";
+
+        BuildDateLabel.Text = ReadTextFileAsync(filePath).Result;
+
+        //await LoadMauiAsset();
+
+    }
+
+    public async Task<string> ReadTextFileAsync(string filePath)
+    {
+        using Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync(filePath);
+        using StreamReader reader = new StreamReader(fileStream);
+
+        return await reader.ReadToEndAsync();
+    }
+
+    async Task LoadMauiAsset()
+    {
+        using var stream = await FileSystem.OpenAppPackageFileAsync("AboutAssets.txt");
+        using var reader = new StreamReader(stream);
+
+        var contents = reader.ReadToEnd();
+    }
 }
--- AboutButton.xaml.cs
+++ AboutButton.xaml.cs
@@ -7,10 +7,23 @@ public partial class AboutButton : ContentPage
 		InitializeComponent();
 	}
 
-    private void ReadFile_Click(object sender, EventArgs e)
+    private async void ReadFile_Click(object sender, EventArgs e)
     {
-        DisplayAlert("ReadFile", $"You made it to the Read File Method", "OK");
+        //DisplayAlert("ReadFile", $"You made it to the Read File Method", "OK");
 
-        
+        string filePath = "AboutAssets.txt";
+
+        BuildDateLabel2.Text = await ReadTextFileAsync(filePath);
+
+
+
     } // end ReadFile
- }
+
+    public async Task<string> ReadTextFileAsync(string filePath)
+    {
+        using Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync(filePath);
+        using StreamReader reader = new StreamReader(fileStream);
+
+        return await reader.ReadToEndAsync();
+    }
+}
